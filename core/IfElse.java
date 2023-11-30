756914fc30241dd01b2c888a258ad4e949743539
SanjaySokal <sokalsanjay@gmail.com>
2023-11-30 07:09:30 +0530

    23-oct-2023 assignment

---
+++ core/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+        if (condition1) {
+            //
+        } else if (condition2) {
+            //
+        } else {
+            //
+        }
+    }
+}
